237b5e1d74ce0230fa74c9a9565505eb8cab8894
SebastianFournier <sebastian.fournier@multitravel.com>
2023-07-23 19:49:39 -0300

    First commit docker

---
+++ src/api/vuelo/routes/vuelo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * vuelo router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::vuelo.vuelo');
---
+++ src/api/vuelo/services/vuelo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * vuelo service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::vuelo.vuelo');
